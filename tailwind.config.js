067c88c985494ede556fe638f316867566bedd4d
서원준 (스원준) <cwoos311@gmail.com>
2026-03-15 04:18:19 +0000

    File changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
   	extend: {
+  		fontFamily: {
+  			main: ['var(--font-main)']
+  		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
