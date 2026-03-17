bacf0ed4906ac92408d5ab2cf05c6805aaeee494
서원준 (스원준) <cwoos311@gmail.com>
2026-03-17 08:57:14 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -4,7 +4,7 @@ import BottomNav from './BottomNav';
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen bg-background max-w-lg mx-auto relative">
+    <div className="min-h-screen bg-background max-w-lg mx-auto relative" style={{ paddingTop: 'env(safe-area-inset-top)' }}>
       <main className="pb-20">
         <Outlet />
       </main>
