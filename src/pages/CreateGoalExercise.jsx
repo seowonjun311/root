37fb3a055a954f011924f71f102c292788cd55be
서원준 (스원준) <cwoos311@gmail.com>
2026-03-19 06:44:59 +0000

    File changes

---
+++ src/pages/CreateGoalExercise.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import CreateGoalForm from '@/components/creategoal/CreateGoalForm';
+
+export default function CreateGoalExercise() {
+  return <CreateGoalForm category="exercise" />;
+}
---
+++ src/pages/CreateGoalStudy.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import CreateGoalForm from '@/components/creategoal/CreateGoalForm';
+
+export default function CreateGoalStudy() {
+  return <CreateGoalForm category="study" />;
+}
