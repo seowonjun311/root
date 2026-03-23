b7ecb23191e77e690c8f66740efbe16c4ad13298
seowonjun311 <cwoos311@gmail.com>
2026-03-23 09:54:46 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-iimport React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
