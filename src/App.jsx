0275840d96bbde48a540cff8b29b079676dc20c0
seowonjun311 <cwoos311@gmail.com>
2026-03-31 18:06:16 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, lazy, Suspense } from 'react';
-import { Toaster } from '@/components/ui/toaster';
+import { Toaster } from 'sonner';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClientInstance } from '@/lib/query-client';
 import { BrowserRouter as Router, Route, Routes, useLocation, Navigate, useNavigate } from 'react-router-dom';
@@ -315,7 +315,7 @@ function App() {
             <AnimationStateProvider>
               <TabNavigationProvider>
                 <AppRoutes />
-                <Toaster />
+                <Toaster richColors position="top-center" />
               </TabNavigationProvider>
             </AnimationStateProvider>
           </NavigationProvider>
